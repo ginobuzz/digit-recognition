94ff1884dc63704c3089a2022726153ee2426347
ginobuzz <ginobuzz@nickelback.cse.buffalo.edu>
2013-11-20 17:01:06 -0500

    removed autosave files

--- train_lr.m
+++ train_lr.m
@@ -2,14 +2,5 @@ function [X] = train_lr()
 %TRAIN_LR Summary of this function goes here
 %   Detailed explanation goes here
 
-    % Import Training Sets
-    D = load('features_train/0.txt');
-    [M,N] = size(D);
-    X = zeros(M,N+1);
-    
-    for i = 1:M
-        label = i -1;
-        X(i,:) = [label D(i,:)];
-    end
 
 end
