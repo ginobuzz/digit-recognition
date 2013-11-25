9c23e33810b4d0443b89831aa1c748b777ecf8e5
ginobuzz <ggbuzzelli@gmail.com>
2013-11-25 17:50:18 -0500

    Update train_lr.m

--- train_lr.m
+++ train_lr.m
@@ -1,4 +1,4 @@
-function [Theta] = train_lr()
+function W = train_lr()
 %==========================================================================
 % train_lr: Linear Regression Classifier
 %
@@ -6,38 +6,37 @@ function [Theta] = train_lr()
 %       none
 %
 %   Output:
-%       P - Parameter Matrix.
+%       W - Parameter Matrix.
 %
 %   Author: ginobuzz
 %==========================================================================
 
-    % Error threshold; Convergence criteria.
-    E = 0.1;
-    
-    
-    [X, Y] = formatData();  % Initialize training matrix and labels.
-    P = rand(10, 513);      % Initialize parameter matrix.
+    % Convergence criteria
+    E = 0.01;
 
-    
+    % Number of classes.
+    M = 10
+
+
+    % Initialize training matrix, boolean target matrix, and label vector. 
+    [X,T,L] = formatData('features_train/');
+    [N,D]   = size(X);
+
+    % Initialize parameter matrix.
+    W = zeros(D,M);
+
     error = 100;
     while error > E
-        H = hypothesis_lr(X,Y,P); % Build hypothesis matrix.
-        P = gradientDescent(X,Y,P,H);
-        error = 0.01;
+
+        % Build hypothesis matrix.
+        Y = hypothesis(X,W);
+
+        % Get new parameter matrix, using gradient descent.
+        W_new = gradientDescent(X,T,W,Y);
+
+        % Update error.
+        %! CHANGE 
+        error = 0;
     end
-    
-    
-    
 
-
-
-    function c = cost( H )
-        arg1 = (-y) * log(h);
-        arg2 = (1 - y) * log(1 - h);
-        c = arg1 - arg2;
-    end
-    
 end
-
-
-
