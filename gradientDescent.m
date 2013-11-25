5e4bcc186a2ff8f667e475de137aee0d76f8ae5f
ginobuzz <ggbuzzelli@gmail.com>
2013-11-25 18:13:44 -0500

    Update gradientDescent.m

--- gradientDescent.m
+++ gradientDescent.m
@@ -1,21 +1,23 @@
-function W_new = gradientDescent( X, T, W, Y )
+function [W_new, error] = gradientDescent( X, T, W, Y )
 
     ALPHA = 0.0001;
-
 
     % Initialize new W matrix.
     [D,M] = size(W);
     W_new = zeros(D,M);
 
+    error = 0;
+
     for i = 1:D
     
-        cost    = Y(i,:) - T(i,:);
-        regCost = ALPHA * cost;
-        descent = cost' * X(i,:);  
+        diff    = Y(i,:) - T(i,:);
+        cost    = diff' * X(i,:);
+        descent = ALPHA * cost;  
+
+        error = error + cost;
 
         W_new(i,:) = W(i,:) - descent;
     
     end
 
 end
-
